be95bb82649b4718dfef9e7602aa022f6bd5c0bb
Dhruvin Shah <dhruvin3@gmail.com>
2023-08-09 21:56:06 -0400

    demo: usage of tailwind css added with hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="flex items-center h-[100vh] w-full">
+      <section className="flex justify-center w-full p-4 bg-white bg-opacity-20">
+        Hello World
+      </section>
+    </div>
+  );
 }
 
 export default App;
